e890bd228837779a349da590fe139ca4aec206fb
AWSNB <nafea@amazon.com>
2016-11-17 23:20:59 -0800

    Create prepare_new_cl.sh

---
+++ hdk/cl/developer_designs/prepare_new_cl.sh
@@ -0,0 +1,6 @@
+# TODO
+# Check if /build and /design directories exist, abort
+# Check if $HDK_COMMON_DIR exist
+
+cp -r $HDK_COMMON_DIR/new_cl_template/build .
+cp -r $HDK_COMMON_DIR/new_cl_template/design .
