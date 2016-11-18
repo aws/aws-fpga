6895316b1c4b329cf7974ac36a97d6be614edb0b
Winefred Washington <winefred@amazon.com>
2016-11-17 17:46:31 -0800

    more directory moves
    Change-Id: I1bbb65176d3af39327db87ed7c377941d9a346bf

--- hdk/cl/examples/cl_simple/build/scripts/encrypt.tcl
+++ hdk/cl/examples/cl_simple/build/scripts/encrypt.tcl
@@ -12,19 +12,19 @@
 #---- User would replae this section with design files ----
 
 ## Change file names and paths below to reflect your user area.  DO NOT include AWS RTL files.
-file copy -force $RTL_ORIGIN/cl/cl_simple_defines.vh ../src
-file copy -force $RTL_ORIGIN/cl/cl_simple.sv ../src
-file copy -force $RTL_ORIGIN/cl/cl_tst.sv ../src
-file copy -force $RTL_ORIGIN/cl/cl_int_tst.sv ../src
-file copy -force $RTL_ORIGIN/cl/mem_scrb.sv ../src
-file copy -force $RTL_ORIGIN/cl/cl_tst_scrb.sv ../src
+file copy -force $RTL_ORIGIN/cl/cl_simple_defines.vh ../src_post_encryption
+file copy -force $RTL_ORIGIN/cl/cl_simple.sv ../src_post_encryption
+file copy -force $RTL_ORIGIN/cl/cl_tst.sv ../src_post_encryption
+file copy -force $RTL_ORIGIN/cl/cl_int_tst.sv ../src_post_encryption
+file copy -force $RTL_ORIGIN/cl/mem_scrb.sv ../src_post_encryption
+file copy -force $RTL_ORIGIN/cl/cl_tst_scrb.sv ../src_post_encryption
 
 encrypt -k keyfile.txt -lang verilog \
-../src/cl_simple_defines.vh \
-../src/cl_simple.sv \
-../src/cl_tst.sv  \
-../src/mem_scrb.sv  \
-../src/cl_tst_scrb.sv  \
-../src/cl_int_tst.sv  
+../src_post_encryption/cl_simple_defines.vh \
+../src_post_encryption/cl_simple.sv \
+../src_post_encryption/cl_tst.sv  \
+../src_post_encryption/mem_scrb.sv  \
+../src_post_encryption/cl_tst_scrb.sv  \
+../src_post_encryption/cl_int_tst.sv  
 
 #---- End of section replaced by User ---
