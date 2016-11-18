e4c1aea5e59a17c0aad65b518f17b88581378a2a
AWSNB <nafea@amazon.com>
2016-11-17 22:27:16 -0800

    Update hdk_setup.sh

--- hdk/hdk_setup.sh
+++ hdk/hdk_setup.sh
@@ -1,1 +1,7 @@
-if ($HDK_DIR)
+export HDK_DIR=${HDK_DIR:=`pwd`}
+
+# The next variable should not be modified and should always point to the /common directory that's provided by HDK
+export HDK_AWS_DIR=$HDK_DIR/common
+
+# The CL_DIR is where the actual Custom Logic design reside, the developer is expected to override this
+export CL_DIR=$HDK_DIR/cl/developer_designs
