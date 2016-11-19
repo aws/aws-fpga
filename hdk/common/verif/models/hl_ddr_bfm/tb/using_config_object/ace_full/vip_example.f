c170a99b973c9711c2f03d0b1ea98c5901e3068d
Winefred Washington <winefred@amazon.com>
2016-11-19 07:31:48 -0800

    removing hl_ddr_bfm from repo
    Change-Id: I29685b33c5b495a314344d9071485e34598e84ed

--- hdk/common/verif/models/hl_ddr_bfm/tb/using_config_object/ace_full/vip_example.f
+++
@@ -1,5 +0,0 @@
-PROTOCOLS=ace
-SOURCES=../../../../../hdl_interfaces/cdn_axi/cdnAceFullInterface.sv aceFullUvmUserTop.sv aceFullUvmUserTb.sv
-TOPMODULE=testbench
-UVM_TESTNAME=basicTest
-OPTIONS_COMP_vcs=-timescale=1ns/10ps  
