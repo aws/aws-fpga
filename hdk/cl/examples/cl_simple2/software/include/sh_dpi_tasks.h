48334031a0151e7b5b06462badb19361e29fa2fc
Winefred Washington <winefred@amazon.com>
2016-11-25 09:20:16 -0800

    adding support for user defined C files.
    Change-Id: I478e443d2febe95f3deec01d076b197c394ac8c7

---
+++ hdk/cl/examples/cl_simple2/software/include/sh_dpi_tasks.h
@@ -0,0 +1,22 @@
+#ifndef SH_DPI_TASKS
+#define SH_DPI_TASKS
+
+#include <stdarg.h>
+
+extern void sv_printf(char *msg);
+extern void cl_peek(long long addr, int *data);
+extern void cl_poke(long long addr, int  data);
+
+void log_printf(const char *format, ...)
+{
+  static char sv_msg_buffer[256];
+  va_list args;
+
+  va_start(args, format);
+  vsprintf(sv_msg_buffer, format, args);
+  sv_printf(sv_msg_buffer);
+
+  va_end(args);
+}
+
+#endif
