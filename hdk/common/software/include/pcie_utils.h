1c14b285b02d802efe2cf2d70214132f193e08bb
Winefred Washington <winefred@amazon.com>
2016-11-28 09:38:04 -0800

    adding common header file for pcie utilities
    Change-Id: If52a5ffc2464d95344935def12d64b82fe8b5807

---
+++ hdk/common/software/include/pcie_utils.h
@@ -0,0 +1,8 @@
+// =============================================================================
+// Copyright 2016 Amazon.com, Inc. or its affiliates.
+// All Rights Reserved Worldwide.
+// =============================================================================
+
+void poke(uint32_t addr, uint32_t value);
+uint32_t peek(uint32_t addr);
+bool pcie_connect(int *argcP, char ***argvP);
