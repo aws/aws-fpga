a55ab93dcf4eb6a6c889af78e4940a235c7cb139
Hima Sajja <himasajj@amazon.com>
2017-01-11 15:59:17 -0800

    Added timeouts
    Change-Id: I5e74b8aadfcdf830630d8d41b62257a5c058f5ff

--- hdk/cl/examples/cl_xdma/verif/tests/test_xdma.sv
+++ hdk/cl/examples/cl_xdma/verif/tests/test_xdma.sv
@@ -1,5 +1,7 @@
 module test_xdma();
-
+    logic status;
+    int   timeout_count;
+   
     initial begin
 
        logic [7:0] desc_buf [];
@@ -14,12 +16,18 @@ module test_xdma();
 
        for (int i = 5 ; i<= 63 ; i++) begin
          desc_buf[i] = 'h0;
-    
        end
-       que_buffer_to_cl(0, desc_buf, 64);
        
-       start_dma_to_cl(0);
+       tb.sh.que_buffer_to_cl(0, desc_buf, 64);
+       
+       tb.sh.start_dma_to_cl(0);
 
+       timeout_count = 0;
+       do begin
+         status = tb.sh.is_dma_to_cl_done(0);
+         timeout_count++;
+       end while ((!status) && (timeout_count < 100));
+       
        desc_buf[0] = 8'h5;
        desc_buf[1] = 8'h6;
        desc_buf[2] = 8'h7;
@@ -30,10 +38,15 @@ module test_xdma();
           desc_buf[i] = 'h0;
        end
 
-       que_cl_to_buffer(0, desc_buf, 64);
+       tb.sh.que_cl_to_buffer(0, desc_buf, 64);
        
-       start_dma_tp_buffer(0);
+       tb.sh.start_dma_to_buffer(0);
 
+       timeout_count = 0;
+       do begin
+         status = tb.sh.is_dma_to_buffer_done(0);
+         timeout_count++;
+       end while ((!status) && (timeout_count < 100));
     end // initial begin
 
 endmodule // test_xdma
