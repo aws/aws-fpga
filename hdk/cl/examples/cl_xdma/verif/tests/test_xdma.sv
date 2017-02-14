b661e6bbcb5074dd567b9c916230ff5fb87d630c
sagsonar <sagsonar@amazon.com>
2017-02-14 15:00:23 -0600

    Changes the test to do writes and read on single channel
    Change-Id: I75a24fff9cacdbeeb273116cfe89181fe0e1258c

--- hdk/cl/examples/cl_xdma/verif/tests/test_xdma.sv
+++ hdk/cl/examples/cl_xdma/verif/tests/test_xdma.sv
@@ -37,7 +37,7 @@ module test_xdma();
          desc_buf[i] = 'hBB;
        end
    
-       que_to_cl_ddr(1, 64'h100100, desc_buf);
+       que_to_cl_ddr(0, 64'h100100, desc_buf);
 
        // DDR 2
        $display("[%t] : DMA buffer to DDR 2", $realtime);
@@ -46,7 +46,7 @@ module test_xdma();
          desc_buf[i] = 'hCC;
        end
    
-       que_to_cl_ddr(2, 64'h100200, desc_buf);
+       que_to_cl_ddr(0, 64'h100200, desc_buf);
 
        // DDR 3
        $display("[%t] : DMA buffer to DDR 3", $realtime);
@@ -55,7 +55,7 @@ module test_xdma();
          desc_buf[i] = 'hDD;
        end
    
-       que_to_cl_ddr(3, 64'h100300, desc_buf);
+       que_to_cl_ddr(0, 64'h100300, desc_buf);
        
        // DDR 0
        $display("[%t] : DMA buffer from DDR 0", $realtime);
@@ -67,7 +67,7 @@ module test_xdma();
            error_count++;
          end    
        end
-/*       
+       
        // DDR 1
        $display("[%t] : DMA buffer from DDR 1", $realtime);
    
@@ -100,7 +100,7 @@ module test_xdma();
            error_count++;
          end    
        end
-*/       
+       
        // Power down
        #500ns;
        tb.sh.power_down();
