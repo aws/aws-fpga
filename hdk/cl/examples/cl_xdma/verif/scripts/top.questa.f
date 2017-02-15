e6484230f27e5b304793e024fc7d1cbe36871920
sagsonar <sagsonar@amazon.com>
2017-02-15 17:31:07 -0600

    making cl_xdma test work with questa
    Change-Id: Idc1d6aa9786767904bc26c88ac1c4cae8cc43f36

--- hdk/cl/examples/cl_xdma/verif/scripts/top.questa.f
+++ hdk/cl/examples/cl_xdma/verif/scripts/top.questa.f
@@ -11,7 +11,6 @@
 +libext+.svh
 
 -y ${CL_ROOT}/design
--y ${CL_ROOT}/design/axi_crossbar_1/hdl  
 -y ${SH_LIB_DIR}
 -y ${SH_INF_DIR}
 
@@ -19,20 +18,27 @@
 +incdir+${SH_LIB_DIR}
 +incdir+${SH_INF_DIR}
 +incdir+${HDK_COMMON_DIR}/verif/include
++incdir+${CL_ROOT}/design/axi_crossbar_0  
++incdir+${CL_ROOT}/design/src_register_slice  
++incdir+${CL_ROOT}/design/dest_register_slice
 
-${CL_ROOT}/design/axi_crossbar_1/synth/axi_crossbar_1.v
-${CL_ROOT}/design/axi_crossbar_1/hdl/axi_crossbar_v2_1_vl_rfs.v
-${CL_ROOT}/design/axi_crossbar_1/hdl/axi_data_fifo_v2_1_vl_rfs.v
-${CL_ROOT}/design/axi_crossbar_1/hdl/axi_infrastructure_v1_1_vl_rfs.v
-${CL_ROOT}/design/axi_crossbar_1/hdl/axi_register_slice_v2_1_vl_rfs.v
-${CL_ROOT}/design/axi_crossbar_1/hdl/generic_baseblocks_v2_1_vl_rfs.v
-${CL_ROOT}/design/axi_crossbar_1/hdl/fifo_generator_v13_1_rfs.v
+${CL_ROOT}/design/axi_crossbar_0/axi_crossbar_0.v
+${CL_ROOT}/design/axi_crossbar_0/axi_crossbar_v2_1_vl_rfs.v
+${CL_ROOT}/design/axi_crossbar_0/axi_data_fifo_v2_1_vl_rfs.v
+${CL_ROOT}/design/axi_crossbar_0/axi_infrastructure_v1_1_vl_rfs.v
+${CL_ROOT}/design/axi_crossbar_0/axi_register_slice_v2_1_vl_rfs.v
+${CL_ROOT}/design/axi_crossbar_0/generic_baseblocks_v2_1_vl_rfs.v
+${CL_ROOT}/design/axi_crossbar_0/fifo_generator_v13_1_rfs.v
 
+${CL_ROOT}/design/axil_slave.sv
 ${CL_ROOT}/design/cl_xdma_defines.vh
 ${CL_ROOT}/design/cl_tst_scrb.sv
 ${CL_ROOT}/design/cl_tst.sv
 ${CL_ROOT}/design/cl_int_tst.sv
 ${CL_ROOT}/design/cl_xdma.sv
+
+${CL_ROOT}/design/src_register_slice/src_register_slice.v
+${CL_ROOT}/design/dest_register_slice/dest_register_slice.v
 
 -f ${HDK_COMMON_DIR}/verif/tb/filelists/tb.${SIMULATOR}.f
 
