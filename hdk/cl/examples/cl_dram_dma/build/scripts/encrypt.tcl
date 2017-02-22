1916ac451b01b6b444ceb9afd9ec16c844a8307a
sagsonar <sagsonar@amazon.com>
2017-02-22 10:46:01 -0600

    Adding fixes to get the synth build work
    Change-Id: Ia4d68c67331dc6e58c3ff3d04154d5e943d77685

--- hdk/cl/examples/cl_dram_dma/build/scripts/encrypt.tcl
+++ hdk/cl/examples/cl_dram_dma/build/scripts/encrypt.tcl
@@ -23,13 +23,25 @@ file copy -force $CL_DIR/design/cl_tst.sv $CL_DIR/build/src_post_encryption
 file copy -force $CL_DIR/design/cl_int_tst.sv $CL_DIR/build/src_post_encryption
 file copy -force $CL_DIR/design/mem_scrb.sv $CL_DIR/build/src_post_encryption
 file copy -force $CL_DIR/design/cl_tst_scrb.sv $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axil_slave.sv $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/src_register_slice/src_register_slice.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/dest_register_slice/dest_register_slice.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/axi_crossbar_0.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/axi_register_slice_v2_1_vl_rfs.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/axi_crossbar_v2_1_vl_rfs.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/axi_data_fifo_v2_1_vl_rfs.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/axi_infrastructure_v1_1_0.vh $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/axi_infrastructure_v1_1_vl_rfs.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/generic_baseblocks_v2_1_vl_rfs.v $CL_DIR/build/src_post_encryption
+file copy -force $CL_DIR/design/axi_crossbar_0/fifo_generator_v13_1_rfs.v $CL_DIR/build/src_post_encryption
 
-encrypt -k $HDK_SHELL_DIR/build/scripts/vivado_keyfile.txt -lang verilog \
-$CL_DIR/build/src_post_encryption/cl_dram_dma_defines.vh \
-$CL_DIR/build/src_post_encryption/cl_dram_dma.sv \
-$CL_DIR/build/src_post_encryption/cl_tst.sv  \
-$CL_DIR/build/src_post_encryption/mem_scrb.sv  \
-$CL_DIR/build/src_post_encryption/cl_tst_scrb.sv  \
-$CL_DIR/build/src_post_encryption/cl_int_tst.sv  
+
+#encrypt -k $HDK_SHELL_DIR/build/scripts/vivado_keyfile.txt -lang verilog \
+#$CL_DIR/build/src_post_encryption/cl_dram_dma_defines.vh \
+#$CL_DIR/build/src_post_encryption/cl_dram_dma.sv \
+#$CL_DIR/build/src_post_encryption/cl_tst.sv  \
+#$CL_DIR/build/src_post_encryption/mem_scrb.sv  \
+#$CL_DIR/build/src_post_encryption/cl_tst_scrb.sv  \
+#$CL_DIR/build/src_post_encryption/cl_int_tst.sv  
 
 #---- End of section replaced by Developr ---
