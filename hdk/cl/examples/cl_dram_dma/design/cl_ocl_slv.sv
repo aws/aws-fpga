2ddda53b5abcd4a4b6555b4a7fec24d3b06a0b30
Sundeep Amirineni <amirinen@amazon.com>
2017-02-27 10:30:49 -0600

    more cleanup of cl_dram_dma
    Change-Id: I8b77a19ad83a9da6560babaeec5d892639465565

--- hdk/cl/examples/cl_dram_dma/design/cl_ocl_slv.sv
+++ hdk/cl/examples/cl_dram_dma/design/cl_ocl_slv.sv
@@ -5,55 +5,19 @@ module cl_ocl_slv (
 
    input sh_cl_flr_assert_q,
 
-   input [31:0] sh_ocl_awaddr,
-   input sh_ocl_awvalid,
-   output logic ocl_sh_awready,
-
-   input [31:0] sh_ocl_wdata,
-   input [3:0] sh_ocl_wstrb,
-   input sh_ocl_wvalid,
-   output logic ocl_sh_wready,
-
-   output logic [1:0] ocl_sh_bresp,
-   output logic ocl_sh_bvalid,
-   input sh_ocl_bready,
-
-   input [31:0] sh_ocl_araddr,
-   input sh_ocl_arvalid,
-   output logic ocl_sh_arready,
-
-   output logic [31:0] ocl_sh_rdata,
-   output logic [1:0] ocl_sh_rresp,
-   output logic ocl_sh_rvalid,
-   input sh_ocl_rready,
+   axi_bus_t sh_ocl_bus,
 
    cfg_bus_t pcim_tst_cfg_bus,
-   cfg_bus_t ddr0_tst_cfg_bus,
-   cfg_bus_t ddr1_tst_cfg_bus,
-   cfg_bus_t ddr2_tst_cfg_bus,
-   cfg_bus_t ddr3_tst_cfg_bus,
+   cfg_bus_t ddra_tst_cfg_bus,
+   cfg_bus_t ddrb_tst_cfg_bus,
+   cfg_bus_t ddrc_tst_cfg_bus,
+   cfg_bus_t ddrd_tst_cfg_bus,
    cfg_bus_t int_tst_cfg_bus
 
 );
 
 
-logic sh_ocl_awvalid_q;
-logic[31:0] sh_ocl_awaddr_q;
-logic ocl_sh_awready_q;
-logic sh_ocl_wvalid_q;
-logic[31:0] sh_ocl_wdata_q;
-logic[3:0] sh_ocl_wstrb_q;
-logic ocl_sh_wready_q;
-logic ocl_sh_bvalid_q;
-logic[1:0] ocl_sh_bresp_q;
-logic sh_ocl_bready_q;
-logic sh_ocl_arvalid_q;
-logic[31:0] sh_ocl_araddr_q;
-logic ocl_sh_arready_q;
-logic ocl_sh_rvalid_q;
-logic[31:0] ocl_sh_rdata_q;
-logic[1:0] ocl_sh_rresp_q;
-logic sh_ocl_rready_q;
+axi_bus_t sh_ocl_bus_q();
 
 
    axi4_flop_fifo #(.IN_FIFO(1), .ADDR_WIDTH(32), .DATA_WIDTH(32), .ID_WIDTH(1), .A_USER_WIDTH(1), .FIFO_DEPTH(3)) AXIL_OCL_REG_SLC (
@@ -61,65 +25,66 @@ logic sh_ocl_rready_q;
     .aresetn       (sync_rst_n),
     .sync_rst_n    (1'b1),
     .s_axi_awid    (1'b0),
-    .s_axi_awaddr  (sh_ocl_awaddr),
+    .s_axi_awaddr  (sh_ocl_bus.awaddr[31:0]),
     .s_axi_awlen   (8'h00),                                            
-    .s_axi_awvalid (sh_ocl_awvalid),
+    .s_axi_awvalid (sh_ocl_bus.awvalid),
     .s_axi_awuser  (1'b0),
-    .s_axi_awready (ocl_sh_awready),
-    .s_axi_wdata   (sh_ocl_wdata),
-    .s_axi_wstrb   (sh_ocl_wstrb),
+    .s_axi_awready (sh_ocl_bus.awready),
+    .s_axi_wdata   (sh_ocl_bus.wdata[31:0]),
+    .s_axi_wstrb   (sh_ocl_bus.wstrb[3:0]),
     .s_axi_wlast   (1'b0),
     .s_axi_wuser   (1'b0),
-    .s_axi_wvalid  (sh_ocl_wvalid),
-    .s_axi_wready  (ocl_sh_wready),
+    .s_axi_wvalid  (sh_ocl_bus.wvalid),
+    .s_axi_wready  (sh_ocl_bus.wready),
     .s_axi_bid     (),
-    .s_axi_bresp   (ocl_sh_bresp),
-    .s_axi_bvalid  (ocl_sh_bvalid),
+    .s_axi_bresp   (sh_ocl_bus.bresp),
+    .s_axi_bvalid  (sh_ocl_bus.bvalid),
     .s_axi_buser   (),
-    .s_axi_bready  (sh_ocl_bready),
+    .s_axi_bready  (sh_ocl_bus.bready),
     .s_axi_arid    (1'h0),
-    .s_axi_araddr  (sh_ocl_araddr),
+    .s_axi_araddr  (sh_ocl_bus.araddr[31:0]),
     .s_axi_arlen   (8'h0), 
-    .s_axi_arvalid (sh_ocl_arvalid),
+    .s_axi_arvalid (sh_ocl_bus.arvalid),
     .s_axi_aruser  (1'd0),
-    .s_axi_arready (ocl_sh_arready),
+    .s_axi_arready (sh_ocl_bus.arready),
     .s_axi_rid     (),
-    .s_axi_rdata   (ocl_sh_rdata),
-    .s_axi_rresp   (ocl_sh_rresp),
+    .s_axi_rdata   (sh_ocl_bus.rdata[31:0]),
+    .s_axi_rresp   (sh_ocl_bus.rresp),
     .s_axi_rlast   (),
     .s_axi_ruser   (),
-    .s_axi_rvalid  (ocl_sh_rvalid),
-    .s_axi_rready  (sh_ocl_rready), 
+    .s_axi_rvalid  (sh_ocl_bus.rvalid),
+    .s_axi_rready  (sh_ocl_bus.rready),
+ 
     .m_axi_awid    (),
-    .m_axi_awaddr  (sh_ocl_awaddr_q), 
+    .m_axi_awaddr  (sh_ocl_bus_q.awaddr[31:0]), 
     .m_axi_awlen   (),
-    .m_axi_awvalid (sh_ocl_awvalid_q),
+    .m_axi_awvalid (sh_ocl_bus_q.awvalid),
     .m_axi_awuser  (),
-    .m_axi_awready (ocl_sh_awready_q),
-    .m_axi_wdata   (sh_ocl_wdata_q),  
-    .m_axi_wstrb   (sh_ocl_wstrb_q),
-    .m_axi_wvalid  (sh_ocl_wvalid_q), 
+    .m_axi_awready (sh_ocl_bus_q.awready),
+    .m_axi_wdata   (sh_ocl_bus_q.wdata[31:0]),  
+    .m_axi_wstrb   (sh_ocl_bus_q.wstrb[3:0]),
+    .m_axi_wvalid  (sh_ocl_bus_q.wvalid), 
     .m_axi_wlast   (),
     .m_axi_wuser   (),
-    .m_axi_wready  (ocl_sh_wready_q), 
-    .m_axi_bresp   (ocl_sh_bresp_q),  
-    .m_axi_bvalid  (ocl_sh_bvalid_q), 
+    .m_axi_wready  (sh_ocl_bus_q.wready), 
+    .m_axi_bresp   (sh_ocl_bus_q.bresp),  
+    .m_axi_bvalid  (sh_ocl_bus_q.bvalid), 
     .m_axi_bid     (),
     .m_axi_buser   (1'b0),
-    .m_axi_bready  (sh_ocl_bready_q), 
+    .m_axi_bready  (sh_ocl_bus_q.bready), 
     .m_axi_arid    (), 
-    .m_axi_araddr  (sh_ocl_araddr_q), 
+    .m_axi_araddr  (sh_ocl_bus_q.araddr[31:0]), 
     .m_axi_arlen   (), 
     .m_axi_aruser  (), 
-    .m_axi_arvalid (sh_ocl_arvalid_q),
-    .m_axi_arready (ocl_sh_arready_q),
+    .m_axi_arvalid (sh_ocl_bus_q.arvalid),
+    .m_axi_arready (sh_ocl_bus_q.arready),
     .m_axi_rid     (),  
-    .m_axi_rdata   (ocl_sh_rdata_q),  
-    .m_axi_rresp   (ocl_sh_rresp_q),  
+    .m_axi_rdata   (sh_ocl_bus_q.rdata[31:0]),  
+    .m_axi_rresp   (sh_ocl_bus_q.rresp),  
     .m_axi_rlast   (),  
     .m_axi_ruser   (1'b0),
-    .m_axi_rvalid  (ocl_sh_rvalid_q), 
-    .m_axi_rready  (sh_ocl_rready_q)
+    .m_axi_rvalid  (sh_ocl_bus_q.rvalid), 
+    .m_axi_rready  (sh_ocl_bus_q.rready)
    );
 
 
@@ -165,10 +130,10 @@ logic slv_did_req;            //Once cycle request, latch that did the request
 
 
 //Write request valid when both address is valid
-assign slv_wr_req = sh_ocl_awvalid_q;
-assign slv_rd_req = sh_ocl_arvalid_q;
-assign slv_mx_rsp_ready = (slv_cyc_wr)? sh_ocl_bready_q: sh_ocl_rready_q;
-assign slv_mx_req_valid = (slv_cyc_wr)?   sh_ocl_wvalid_q: 1'b1;
+assign slv_wr_req = sh_ocl_bus_q.awvalid;
+assign slv_rd_req = sh_ocl_bus_q.arvalid;
+assign slv_mx_rsp_ready = (slv_cyc_wr)? sh_ocl_bus_q.bready: sh_ocl_bus_q.rready;
+assign slv_mx_req_valid = (slv_cyc_wr)?   sh_ocl_bus_q.wvalid: 1'b1;
 
 //Fixed write hi-pri
 assign slv_arb_wr = slv_wr_req;
@@ -185,9 +150,9 @@ always_ff @(negedge sync_rst_n or posedge clk)
     {slv_req_rd_addr, slv_req_wr_addr} <= 128'd0;
     {slv_req_rd_id, slv_req_wr_id} <= 0;
   end
-  else if ((slv_state == SLV_IDLE) && (sh_ocl_arvalid_q || sh_ocl_awvalid_q))
+  else if ((slv_state == SLV_IDLE) && (sh_ocl_bus_q.arvalid || sh_ocl_bus_q.awvalid))
   begin
-    {slv_req_rd_addr[31:0], slv_req_wr_addr[31:0]} <= {sh_ocl_araddr_q, sh_ocl_awaddr_q};
+    {slv_req_rd_addr[31:0], slv_req_wr_addr[31:0]} <= {sh_ocl_bus_q.araddr, sh_ocl_bus_q.awaddr};
     {slv_req_rd_id, slv_req_wr_id} <= 0;
   end
    
@@ -270,7 +235,7 @@ always_ff @(negedge sync_rst_n or posedge clk)
       for (int i=0; i<NUM_TST; i++)
       begin
          slv_tst_addr[i] <= slv_mx_addr;
-         slv_tst_wdata[i] <= sh_ocl_wdata_q;
+         slv_tst_wdata[i] <= sh_ocl_bus_q.wdata;
       end
    end
 
@@ -315,26 +280,26 @@ always_ff @(negedge sync_rst_n or posedge clk)
 always_ff @(negedge sync_rst_n or posedge clk)
    if (!sync_rst_n)
    begin
-      ocl_sh_awready_q <= 0;
-      ocl_sh_wready_q <= 0;
-      ocl_sh_arready_q <= 0;
+      sh_ocl_bus_q.awready <= 0;
+      sh_ocl_bus_q.wready <= 0;
+      sh_ocl_bus_q.arready <= 0;
    end
    else
    begin
-      ocl_sh_awready_q <= (slv_state_nxt==SLV_WR_ADDR);
-      ocl_sh_wready_q <= ((slv_state==SLV_CYC) && (slv_state_nxt!=SLV_CYC)) && slv_cyc_wr;
-      ocl_sh_arready_q <= ((slv_state==SLV_CYC) && (slv_state_nxt!=SLV_CYC)) && ~slv_cyc_wr;
+      sh_ocl_bus_q.awready <= (slv_state_nxt==SLV_WR_ADDR);
+      sh_ocl_bus_q.wready <= ((slv_state==SLV_CYC) && (slv_state_nxt!=SLV_CYC)) && slv_cyc_wr;
+      sh_ocl_bus_q.arready <= ((slv_state==SLV_CYC) && (slv_state_nxt!=SLV_CYC)) && ~slv_cyc_wr;
    end
    
 //Response back to AXI
-assign ocl_sh_bid_q = slv_req_wr_id;
-assign ocl_sh_bresp_q = 0;
-assign ocl_sh_bvalid_q = (slv_state==SLV_RESP) && slv_cyc_wr;
+assign sh_ocl_bus_q.bid = slv_req_wr_id;
+assign sh_ocl_bus_q.bresp = 0;
+assign sh_ocl_bus_q.bvalid = (slv_state==SLV_RESP) && slv_cyc_wr;
   
-assign ocl_sh_rid_q = slv_req_rd_id;
-assign ocl_sh_rdata_q = slv_rdata;
-assign ocl_sh_rresp_q = 2'b00;
-assign ocl_sh_rvalid_q = (slv_state==SLV_RESP) && !slv_cyc_wr;
+assign sh_ocl_bus_q.rid = slv_req_rd_id;
+assign sh_ocl_bus_q.rdata = slv_rdata;
+assign sh_ocl_bus_q.rresp = 2'b00;
+assign sh_ocl_bus_q.rvalid = (slv_state==SLV_RESP) && !slv_cyc_wr;
 
 
 //assign individual cfg bus
@@ -345,33 +310,33 @@ assign pcim_tst_cfg_bus.rd = slv_tst_rd[0];
 assign tst_slv_ack[0] = pcim_tst_cfg_bus.ack;
 assign tst_slv_rdata[0] = pcim_tst_cfg_bus.rdata;
 
-assign ddr0_tst_cfg_bus.addr = slv_tst_addr[1];
-assign ddr0_tst_cfg_bus.wdata = slv_tst_wdata[1];
-assign ddr0_tst_cfg_bus.wr = slv_tst_wr[1];
-assign ddr0_tst_cfg_bus.rd = slv_tst_rd[1];
-assign tst_slv_ack[1] = ddr0_tst_cfg_bus.ack;
-assign tst_slv_rdata[1] = ddr0_tst_cfg_bus.rdata;
+assign ddra_tst_cfg_bus.addr = slv_tst_addr[1];
+assign ddra_tst_cfg_bus.wdata = slv_tst_wdata[1];
+assign ddra_tst_cfg_bus.wr = slv_tst_wr[1];
+assign ddra_tst_cfg_bus.rd = slv_tst_rd[1];
+assign tst_slv_ack[1] = ddra_tst_cfg_bus.ack;
+assign tst_slv_rdata[1] = ddra_tst_cfg_bus.rdata;
 
-assign ddr1_tst_cfg_bus.addr = slv_tst_addr[2];
-assign ddr1_tst_cfg_bus.wdata = slv_tst_wdata[2];
-assign ddr1_tst_cfg_bus.wr = slv_tst_wr[2];
-assign ddr1_tst_cfg_bus.rd = slv_tst_rd[2];
-assign tst_slv_ack[2] = ddr1_tst_cfg_bus.ack;
-assign tst_slv_rdata[2] = ddr1_tst_cfg_bus.rdata;
+assign ddrb_tst_cfg_bus.addr = slv_tst_addr[2];
+assign ddrb_tst_cfg_bus.wdata = slv_tst_wdata[2];
+assign ddrb_tst_cfg_bus.wr = slv_tst_wr[2];
+assign ddrb_tst_cfg_bus.rd = slv_tst_rd[2];
+assign tst_slv_ack[2] = ddrb_tst_cfg_bus.ack;
+assign tst_slv_rdata[2] = ddrb_tst_cfg_bus.rdata;
 
-assign ddr2_tst_cfg_bus.addr = slv_tst_addr[3];
-assign ddr2_tst_cfg_bus.wdata = slv_tst_wdata[3];
-assign ddr2_tst_cfg_bus.wr = slv_tst_wr[3];
-assign ddr2_tst_cfg_bus.rd = slv_tst_rd[3];
-assign tst_slv_ack[3] = ddr2_tst_cfg_bus.ack;
-assign tst_slv_rdata[3] = ddr2_tst_cfg_bus.rdata;
+assign ddrc_tst_cfg_bus.addr = slv_tst_addr[3];
+assign ddrc_tst_cfg_bus.wdata = slv_tst_wdata[3];
+assign ddrc_tst_cfg_bus.wr = slv_tst_wr[3];
+assign ddrc_tst_cfg_bus.rd = slv_tst_rd[3];
+assign tst_slv_ack[3] = ddrc_tst_cfg_bus.ack;
+assign tst_slv_rdata[3] = ddrc_tst_cfg_bus.rdata;
 
-assign ddr3_tst_cfg_bus.addr = slv_tst_addr[4];
-assign ddr3_tst_cfg_bus.wdata = slv_tst_wdata[4];
-assign ddr3_tst_cfg_bus.wr = slv_tst_wr[4];
-assign ddr3_tst_cfg_bus.rd = slv_tst_rd[4];
-assign tst_slv_ack[4] = ddr3_tst_cfg_bus.ack;
-assign tst_slv_rdata[4] = ddr3_tst_cfg_bus.rdata;
+assign ddrd_tst_cfg_bus.addr = slv_tst_addr[4];
+assign ddrd_tst_cfg_bus.wdata = slv_tst_wdata[4];
+assign ddrd_tst_cfg_bus.wr = slv_tst_wr[4];
+assign ddrd_tst_cfg_bus.rd = slv_tst_rd[4];
+assign tst_slv_ack[4] = ddrd_tst_cfg_bus.ack;
+assign tst_slv_rdata[4] = ddrd_tst_cfg_bus.rdata;
 
 
 assign int_tst_cfg_bus.addr = slv_tst_addr[13];
