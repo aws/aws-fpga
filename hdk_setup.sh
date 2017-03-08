e4a667b5198d9699180ceacaa752e9fa0ffa9786
Allan Carter <cartalla@amazon.com>
2017-03-08 15:30:22 -0600

    Feature/hdk shell dcp in s3 (#155)
    * Allow hdk_setup.sh to be sourced from any directory.
    
    - Fix some inconsistencies in the readme file.
    
    Add error checking to hdk_setup.sh
    
    - Add error checking
    - Clean up messages
    - Remove requirement to run in $AWS_FPGA_REPO_DIR
    - Don't reset CL_DIR if already set to an existing directory.
    
    Updated .gitignore
    
    - Add files generated by example builds.
    - Added files generated by vivado gui
    
    * Save HDK shell's checkpoint in aws-fpga-hdk-resources bucket instead of repo.
    
    - Added upload_shell_checkpoint.sh
    - Download shell checkpoint in hdk_setup.sh.
    - Check version of checkpoint in hkd_setup.sh.
    - Uses sha1 sum of the checkpoint that is stored in the bucket.
    
    * Clean up messages in hdk_setup.sh
    
    - Add debug_msg and reduce some messages to debug messages
    - Give a positive PASSED message at end.
    - Add -d and -h options, usage and help messages.
    
    * Update .gitignore
    
    Simplified pattern for vivado log files to not include paths.

--- hdk_setup.sh
+++ hdk_setup.sh
@@ -1,64 +1,184 @@
-#!/bin/bash
+# Script must be sourced from a bash shell or it will not work
+# When being sourced $0 will be the interactive shell and $BASH_SOURCE_ will contain the script being sourced
+# When being run $0 and $_ will be the same.
+script=${BASH_SOURCE[0]}
+if [ $script == $0 ]; then
+  echo "ERROR: You must source this script"
+  exit 2
+fi
+full_script=$(readlink -f $script)
+script_name=$(basename $full_script)
+script_dir=$(dirname $full_script)
+if [[ ':$AWS_FPGA_REPO_DIR' == ':' ]]; then
+  export AWS_FPGA_REPO_DIR=$script_dir
+fi
 
-echo "AWS FPGA: hdk_setup.sh script will (i) check if Xilinx's vivado is installed, (ii) set up key environment variables HDK_*, and (iii) prepare DRAM controller and PCIe IP modules if they are not already available in your directory."
+debug=0
 
-echo "AWS FPGA: Checking for vivado install:"
- 
+function info_msg {
+  echo -e "AWS FPGA-INFO: $1"
+}
+
+function debug_msg {
+  if [[ $debug == 0 ]]; then
+    return
+  fi
+  echo -e "AWS FPGA-DEBUG: $1"
+}
+
+function err_msg {
+  echo -e >&2 "AWS FPGA-ERROR: - $1"
+}
+
+function usage {
+  echo -e "USAGE: source [\$AWS_FPGA_REPO_DIR/]$script_name [-d|-debug] [-h|-help]"
+}
+
+function help {
+  info_msg "$script_name"
+  info_msg " "
+  info_msg "Sets up the environment for AWS FPGA HDK tools."
+  info_msg " "
+  info_msg "hdk_setup.sh script will:"
+  info_msg "  (1) check if Xilinx's vivado is installed,"
+  info_msg "  (2) set up key environment variables HDK_*, and"
+  info_msg "  (3) Download/update the HDK shell's checkpoint"
+  info_msg "  (4) prepare DRAM controller and PCIe IP modules if they are not already available in your directory."
+  echo " "
+  usage
+}
+
+# Process command line args
+args=( "$@" )
+for (( i = 0; i < ${#args[@]}; i++ )); do
+  arg=${args[$i]}
+  case $arg in
+    -d|-debug)
+      debug=1
+    ;;
+    -h|-help)
+      help
+      return 0
+    ;;
+    *)
+      err_msg "Invalid option: $arg\n"
+      usage
+      return 1
+  esac
+done
+
+debug_msg "Checking for vivado install:"
+
 # before going too far make sure Vivado is available
-vivado -version >/dev/null 2>&1 || { echo >&2 "ERROR - Please install/enable Vivado." ; return 1; }
+vivado -version > /dev/null 2>&1 || { err_msg "Please install/enable Vivado."; return 1; }
 
-#Searching for Vivado version and comparing it with the list of supported versio
+#Searching for Vivado version and comparing it with the list of supported versions
 
 export VIVADO_VER=`vivado -version | grep Vivado | head -1`
 
-echo "AWS FPGA: Found $VIVADO_VER"
+info_msg "Using $VIVADO_VER"
 
-if grep -Fxq "$VIVADO_VER" $(pwd)/hdk/supported_vivado_versions.txt
+if grep -Fxq "$VIVADO_VER" $AWS_FPGA_REPO_DIR/hdk/supported_vivado_versions.txt
 then
-    echo "AWS FPGA: $VIVADO_VER is supported by this HDK release."
+    debug_msg "$VIVADO_VER is supported by this HDK release."
 else
-    echo "AWS FPGA: ERROR - $VIVADO_VER is not supported by this HDK release."
+    err_msg "$VIVADO_VER is not supported by this HDK release."
     return 1
 fi
 
-echo "AWS FPGA: Vivado check successed"
+debug_msg "Vivado check succeeded"
 
-echo "AWS FPGA: Setting up environment variables"
+info_msg "Setting up environment variables"
 
 # Clear environment variables
 unset HDK_DIR
 unset HDK_COMMON_DIR
 unset HDK_SHELL_DIR
-unset CL_DIR
+# Don't unset CL_DIR if designer has already set it.
+#unset CL_DIR
 
-export HDK_DIR=${HDK_DIR:=$(pwd)/hdk}
+export HDK_DIR=$AWS_FPGA_REPO_DIR/hdk
 
 # The next variable should not be modified and should always point to the /common directory under HDK_DIR
 export HDK_COMMON_DIR=$HDK_DIR/common
 
 # Point to the latest version of AWS shell
-export HDK_SHELL_DIR=$HDK_COMMON_DIR/shell_stable
+export HDK_SHELL_DIR=$(readlink -f $HDK_COMMON_DIR/shell_stable)
+hdk_shell_version=$(readlink $HDK_COMMON_DIR/shell_stable)
 
 # The CL_DIR is where the actual Custom Logic design resides. The developer is expected to override this.
 # export CL_DIR=$HDK_DIR/cl/developer_designs
 
-echo "AWS FPGA: Done setting environment variables.";
+debug_msg "Done setting environment variables.";
 
-# Create DDR and PCIe IP models and patch PCIe
-if [ ! -f $HDK_COMMON_DIR/verif/models/ddr4_model/arch_defines.v ]
-then
-  echo "DDR4 model files in "$HDK_COMMON_DIR/verif/models/ddr4_model/" do NOT exist. Running model creation step.";
-  echo "This could take 5-10 minutes, please be patient!";
-  # Run init.sh then clean-up
-  source $HDK_DIR/common/verif/scripts/init.sh
-  echo "Done with model creation step. Cleaning up temporary files.";
-  rm -fr tmp
-  rm vivado*jou
-  rm vivado*log
+# Download correct shell DCP
+info_msg "Using HDK shell version $hdk_shell_version"
+debug_msg "Checking HDK shell's checkpoint version"
+hdk_shell_dir=$HDK_SHELL_DIR/build/checkpoints/from_aws
+hdk_shell=$hdk_shell_dir/SH_CL_BB_routed.dcp
+hdk_shell_s3_bucket=aws-fpga-hdk-resources
+# Download the sha1
+aws s3 cp s3://$hdk_shell_s3_bucket/hdk/$hdk_shell_version/build/checkpoints/from_aws/SH_CL_BB_routed.dcp.sha1 $hdk_shell.sha1 --only-show-errors || { err_msg "Failed to download HDK shell's checkpoint version."; return 2; }
+exp_sha1=$(cat $hdk_shell.sha1)
+debug_msg "  latest   version=$exp_sha1"
+# If shell already downloaded check its sha1
+if [ -e $hdk_shell ]; then
+  act_sha1=$( sha1sum $hdk_shell | awk '{ print $1 }' )
+  debug_msg "  existing version=$act_sha1"
+  if [[ $act_sha1 != $exp_sha1 ]]; then
+    info_msg "HDK shell's checkpoint version is incorrect"
+    info_msg "  Saving old checkpoint to $hdk_shell.back"
+    mv $hdk_shell $hdk_shell.back
+  fi
 else
-  echo "DDR4 model files exist in "$HDK_COMMON_DIR/verif/models/ddr4_model/". Skipping model creation step.";
+  info_msg "HDK shell's checkpoint hasn't been downloaded yet."
 fi
-echo "AWS FPGA: Done with AWS HDK setup.";
-echo "AWS FPGA: ATTENTION: Don't forget to change the CL_DIR variable for the directory of your Custom Logic.";
+if [ ! -e $hdk_shell ]; then
+  s3_hdk_shell=s3://$hdk_shell_s3_bucket/hdk/$hdk_shell_version/build/checkpoints/from_aws/SH_CL_BB_routed.dcp
+  info_msg "Downloading latest HDK shell checkpoint from $s3_hdk_shell"
+  aws s3 cp $s3_hdk_shell $hdk_shell --only-show-errors || { err_msg "HDK shell checkpoint download failed"; return 2; }
+fi
+# Check sha1
+act_sha1=$( sha1sum $hdk_shell | awk '{ print $1 }' )
+if [[ $act_sha1 != $exp_sha1 ]]; then
+  err_msg "Incorrect HDK shell checkpoint version:"
+  err_msg "  expected version=$exp_sha1"
+  err_msg "  actual   version=$act_sha1"
+  err_msg "  There may be an issue with the uploaded checkpoint."
+  return 2
+fi
+info_msg "HDK shell is up-to-date"
 
-
+# Create DDR and PCIe IP models and patch PCIe\
+models_dir=$HDK_COMMON_DIR/verif/models
+ddr4_model_dir=$models_dir/ddr4_model
+if [ ! -f $ddr4_model_dir/arch_defines.v ]; then
+  ddr4_build_dir=$AWS_FPGA_REPO_DIR/ddr4_model_build
+  info_msg "DDR4 model files in "$ddr4_model_dir/" do NOT exist. Running model creation step.";
+  info_msg "  Building in $ddr4_build_dir"
+  info_msg "  This could take 5-10 minutes, please be patient!";
+  mkdir -p $ddr4_build_dir
+  pushd $ddr4_build_dir &> /dev/null
+  # Run init.sh then clean-up
+  if ! $HDK_DIR/common/verif/scripts/init.sh $models_dir; then
+    err_msg "DDR4 model build failed."
+    err_msg "  Build dir=$ddr4_build_dir"
+    popd &> /dev/null
+    return 2
+  fi
+  info_msg "DDR4 model build passed."
+  popd &> /dev/null
+  rm -rf $ddr4_build_dir
+else
+  debug_msg "DDR4 model files exist in "$ddr4_model_dir/". Skipping model creation step.";
+fi
+if [[ ":$CL_DIR" == ':' ]]; then
+  info_msg "ATTENTION: Don't forget to set the CL_DIR variable for the directory of your Custom Logic.";
+else
+  info_msg "CL_DIR is $CL_DIR"
+  if [ ! -d $CL_DIR ]; then
+    err_msg "CL_DIR doesn't exist. Set CL_DIR to a valid directory."
+    unset CL_DIR
+  fi
+fi
+info_msg "AWS HDK setup PASSED.";
