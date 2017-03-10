24b60574103f34ad5c8e809c3a4e4fef28f96367
Allan Carter <cartalla@amazon.com>
2017-03-10 08:43:01 -0600

    On FPGA Developer AMI use the correct vivado version (#167)
    This is to enable having both vivado and sdx installed in the AMI
    so can use both the master and prelease branches which use different
    versions of vivado.
    
    Use module load to use the correct vivado version based on the HDK version.
    
    Save the vivado version used to build the models.
    
    Rebuild the models if they were built with the wrong vivado version.
    
    Add a lock file to prevent multiple process from trying to build the
    models at the same time, potentially with different vivado versions.
    This requires procmail to be installed otherwise the lockfile isn't created/used.
    
    Merged changes from prelease.
    
    Not downloading the shell dcp from S3.
    Leaving that the same.
    This shouldn't be an issue when switching between prelease and master
    because they use different shell versions.

--- hdk_setup.sh
+++ hdk_setup.sh
@@ -1,62 +1,187 @@
-#!/bin/bash
+# Script must be sourced from a bash shell or it will not work
+# When being sourced $0 will be the interactive shell and $BASH_SOURCE_ will contain the script being sourced
+# When being run $0 and $_ will be the same.
+script=${BASH_SOURCE[0]}
+if [ $script == $0 ]; then
+  echo "ERROR: You must source this script"
+  exit 2
+fi
+full_script=$(readlink -f $script)
+script_name=$(basename $full_script)
+script_dir=$(dirname $full_script)
 
-echo "AWS FPGA: hdk_setup.sh script will (i) check if Xilinx's vivado is installed, (ii) set up key environment variables HDK_*, and (iii) prepare DRAM controller and PCIe IP modules if they are not already available in your directory."
+debug=0
 
-echo "AWS FPGA: Checking for vivado install:"
- 
-# before going too far make sure Vivado is available
-vivado -version >/dev/null 2>&1 || { echo >&2 "ERROR - Please install/enable Vivado." ; return 1; }
+function info_msg {
+  echo -e "AWS FPGA-INFO: $1"
+}
 
-export VIVADO_VER=`vivado -version | grep Vivado | head -1`
+function debug_msg {
+  if [[ $debug == 0 ]]; then
+    return
+  fi
+  echo -e "AWS FPGA-DEBUG: $1"
+}
 
-echo "AWS FPGA: Found $VIVADO_VER"
+function err_msg {
+  echo -e >&2 "AWS FPGA-ERROR: $1"
+}
 
-if grep -Fxq "$VIVADO_VER" $(pwd)/hdk/supported_vivado_versions.txt
-then
-    echo "AWS FPGA: $VIVADO_VER is supported by this HDK release."
+function usage {
+  echo -e "USAGE: source [\$AWS_FPGA_REPO_DIR/]$script_name [-d|-debug] [-h|-help]"
+}
+
+function help {
+  info_msg "$script_name"
+  info_msg " "
+  info_msg "Sets up the environment for AWS FPGA HDK tools."
+  info_msg " "
+  info_msg "hdk_setup.sh script will:"
+  info_msg "  (1) check if Xilinx's vivado is installed,"
+  info_msg "  (2) set up key environment variables HDK_*, and"
+  info_msg "  (3) Download/update the HDK shell's checkpoint"
+  info_msg "  (4) prepare DRAM controller and PCIe IP modules if they are not already available in your directory."
+  echo " "
+  usage
+}
+
+# Process command line args
+args=( "$@" )
+for (( i = 0; i < ${#args[@]}; i++ )); do
+  arg=${args[$i]}
+  case $arg in
+    -d|-debug)
+      debug=1
+    ;;
+    -h|-help)
+      help
+      return 0
+    ;;
+    *)
+      err_msg "Invalid option: $arg\n"
+      usage
+      return 1
+  esac
+done
+
+# Make sure that AWS_FPGA_REPO_DIR is set to the location of this script.
+if [[ ":$AWS_FPGA_REPO_DIR" == ':' ]]; then
+  debug_msg "AWS_FPGA_REPO_DIR not set so setting to $script_dir"
+  export AWS_FPGA_REPO_DIR=$script_dir
+elif [[ $AWS_FPGA_REPO_DIR != $script_dir ]]; then
+  info_msg "Changing AWS_FPGA_REPO_DIR from $AWS_FPGA_REPO_DIR to $script_dir"
+  export AWS_FPGA_REPO_DIR=$script_dir
 else
-    echo "AWS FPGA: ERROR - $VIVADO_VER is not supported by this HDK release."
+  debug_msg "AWS_FPGA_REPO_DIR=$AWS_FPGA_REPO_DIR"
+fi
+
+debug_msg "Checking for vivado install:"
+
+# On the FPGA Developer AMI use module load to use the correct version of vivado
+if [ -e /usr/local/Modules/$MODULE_VERSION/bin/modulecmd ]; then
+  # Module command is installed.
+  # This branch requires sdx, not vivado
+  # Load and unload the modules just to make sure have the environment set correctly
+  module unload vivado
+  module unload sdx
+  module load vivado
+fi
+
+# before going too far make sure Vivado is available
+if ! vivado -version > /dev/null 2>&1; then
+    err_msg "Please install/enable Vivado."
+    err_msg "  If you are using the FPGA Developer AMI then please request support."
     return 1
 fi
 
-echo "AWS FPGA: Vivado check successed"
+#Searching for Vivado version and comparing it with the list of supported versions
 
-echo "AWS FPGA: Setting up environment variables"
+export VIVADO_VER=`vivado -version | grep Vivado | head -1`
+
+info_msg "Using $VIVADO_VER"
+
+if grep -Fxq "$VIVADO_VER" $AWS_FPGA_REPO_DIR/hdk/supported_vivado_versions.txt
+then
+    debug_msg "$VIVADO_VER is supported by this HDK release."
+else
+    err_msg "$VIVADO_VER is not supported by this HDK release."
+    err_msg "Supported versions are:"
+    cat $AWS_FPGA_REPO_DIR/hdk/supported_vivado_versions.txt
+    return 1
+fi
+
+debug_msg "Vivado check succeeded"
+
+info_msg "Setting up environment variables"
 
 # Clear environment variables
 unset HDK_DIR
 unset HDK_COMMON_DIR
 unset HDK_SHELL_DIR
-unset CL_DIR
+# Don't unset CL_DIR if designer has already set it.
+#unset CL_DIR
 
-export HDK_DIR=${HDK_DIR:=$(pwd)/hdk}
+export HDK_DIR=$AWS_FPGA_REPO_DIR/hdk
 
 # The next variable should not be modified and should always point to the /common directory under HDK_DIR
 export HDK_COMMON_DIR=$HDK_DIR/common
 
 # Point to the latest version of AWS shell
-export HDK_SHELL_DIR=$HDK_COMMON_DIR/shell_latest
+export HDK_SHELL_DIR=$(readlink -f $HDK_COMMON_DIR/shell_latest)
+hdk_shell_version=$(readlink $HDK_COMMON_DIR/shell_latest)
 
 # The CL_DIR is where the actual Custom Logic design resides. The developer is expected to override this.
 # export CL_DIR=$HDK_DIR/cl/developer_designs
 
-echo "AWS FPGA: Done setting environment variables.";
+debug_msg "Done setting environment variables.";
 
 # Create DDR and PCIe IP models and patch PCIe
-if [ ! -f $HDK_COMMON_DIR/verif/models/ddr4_model/arch_defines.v ]
-then
-  echo "DDR4 model files in "$HDK_COMMON_DIR/verif/models/ddr4_model/" do NOT exist. Running model creation step.";
-  echo "This could take 5-10 minutes, please be patient!";
-  # Run init.sh then clean-up
-  source $HDK_DIR/common/verif/scripts/init.sh
-  echo "Done with model creation step. Cleaning up temporary files.";
-  rm -fr tmp
-  rm vivado*jou
-  rm vivado*log
+models_dir=$HDK_COMMON_DIR/verif/models
+ddr4_model_dir=$models_dir/ddr4_model
+if [ -f $ddr4_model_dir/arch_defines.v ]; then
+  # Models already built
+  # Check to make sure they were built with this version of vivado
+  if [[ -f $models_dir/.vivado_version ]]; then
+    models_vivado_version=$(cat $models_dir/.vivado_version)
+    info_msg "DDR4 model files in $ddr4_model_dir/ were built with $models_vivado_version"
+    if [[ $models_vivado_version != $VIVADO_VER ]]; then
+      info_msg "  Wrong vivado version so rebuilding with $VIVADO_VER"
+    fi
+  else
+    models_vivado_version=UNKNOWN
+    info_msg "DDR4 model files in $ddr4_model_dir/ were built with UNKNOWN vivado version so rebuilding."
+  fi
 else
-  echo "DDR4 model files exist in "$HDK_COMMON_DIR/verif/models/ddr4_model/". Skipping model creation step.";
+  # Models haven't been built
+  models_vivado_version=NOT_BUILT
+  info_msg "DDR4 model files in "$ddr4_model_dir/" do NOT exist. Running model creation step.";
 fi
-echo "AWS FPGA: Done with AWS HDK setup.";
-echo "AWS FPGA: ATTENTION: Don't forget to change the CL_DIR variable for the directory of your Custom Logic.";
-
-
+if [[ $models_vivado_version != $VIVADO_VER ]]; then
+  ddr4_build_dir=$AWS_FPGA_REPO_DIR/ddr4_model_build
+  info_msg "  Building in $ddr4_build_dir"
+  info_msg "  This could take 5-10 minutes, please be patient!";
+  mkdir -p $ddr4_build_dir
+  pushd $ddr4_build_dir &> /dev/null
+  # Run init.sh then clean-up
+  if ! $HDK_DIR/common/verif/scripts/init.sh $models_dir; then
+    err_msg "DDR4 model build failed."
+    err_msg "  Build dir=$ddr4_build_dir"
+    popd &> /dev/null
+    return 2
+  fi
+  info_msg "DDR4 model build passed."
+  popd &> /dev/null
+  rm -rf $ddr4_build_dir
+else
+  debug_msg "DDR4 model files exist in "$ddr4_model_dir/". Skipping model creation step.";
+fi
+if [[ ":$CL_DIR" == ':' ]]; then
+  info_msg "ATTENTION: Don't forget to set the CL_DIR variable for the directory of your Custom Logic.";
+else
+  info_msg "CL_DIR is $CL_DIR"
+  if [ ! -d $CL_DIR ]; then
+    err_msg "CL_DIR doesn't exist. Set CL_DIR to a valid directory."
+    unset CL_DIR
+  fi
+fi
+info_msg "AWS HDK setup PASSED.";
