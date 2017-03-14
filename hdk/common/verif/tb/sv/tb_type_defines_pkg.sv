dcfb753755e0834b58c153bc314350b5e1bd1722
Winefred Washington <winefred@amazon.com>
2017-03-14 11:30:52 -0700

    added clock profile table
    Change-Id: I54d22a5cf0a73265c9e8e2b325d3ebdb64358c83

--- hdk/common/verif/tb/sv/tb_type_defines_pkg.sv
+++ hdk/common/verif/tb/sv/tb_type_defines_pkg.sv
@@ -15,6 +15,23 @@
 
 package tb_type_defines_pkg;
 
+ 
+//                      clk_main_a0	clk_extra_a1	clk_extra_a2	clk_extra_a3
+// Profile 0 (Default)       125        62.5            187.5           250
+// Profile 1	             250       125	            375             500
+// Profile 2                  62.5      31.25           250             125
+// Profile 3                  15.625     7.8125         125              62.5
+// Profile 4                 225       112.5            337.5           450
+// Profile 5                 200       100              300             400
+								
+//                      clk_extra_b0 clk_extra_b1		
+// Profile 0 (Default)       250       125		
+// Profile 1                 125        62.5		
+				
+//                      clk_extra_c0 clk_extra_c1		
+// Profile 0 (Default)       300       400		
+// Profile 1                 150       200		
+ 
   virtual class ClockProfile;     
      typedef enum integer {PROFILE_0=0, PROFILE_1=1, PROFILE_2=2, PROFILE_3=3, PROFILE_4=4, PROFILE_5=5} CLK_PROFILE;
   endclass // Clock_Profile
