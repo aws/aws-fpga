7f04cfd4d8358415fdf0b16c92f3bcaca194a195
Deep Patel <deeppat@amazon.com>
2017-03-30 17:36:12 -0500

    Added the correct header to hdk files

--- hdk/cl/examples/cl_dram_dma/design/mem_scrb.sv
+++ hdk/cl/examples/cl_dram_dma/design/mem_scrb.sv
@@ -1,4 +1,20 @@
-module mem_scrb 
+// Amazon FPGA Hardware Development Kit
+//
+// Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Amazon Software License (the "License"). You may not use
+// this file except in compliance with the License. A copy of the License is
+// located at
+//
+// http://aws.amazon.com/asl/
+//
+// or in the "license" file accompanying this file. This file is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
+// implied. See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+module mem_scrb
   #(ADDR_WIDTH = 64,
     DATA_WIDTH = 512,
     ID_WIDTH = 10,
