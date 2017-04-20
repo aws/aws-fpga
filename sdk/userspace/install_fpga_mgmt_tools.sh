f412778b9f8066a64c30cd237f394c2a05451a16
Jonathan Swinney <jswinney@amazon.com>
2017-04-20 16:52:28 -0500

    enhanced sdk install script for increased portability
    Various Linux distributions have different preferred paths for installations of
    unmanaged user binaries and libraries. This change attempts to follow those
    preferences while maintaining maximum portability.
    
    This script has been tested recent versions (at time of writing) of:
     - Amazon Linux 2017.03
     - Centos (FPGA Dev AMI 1.2)
     - Ubuntu 16.04.2 LTS
     - SUSE 12
    
    It has not been tested on RHEL, but is presumed to work since it is similar
    to Amazon Linux and Centos.
    
    It installs binaries to /usr/local/bin if it's on the sudo PATH or /usr/bin
    otherwise. It installs the shared library the first directory it finds searching
    from the follwing list:
     /usr/local/lib64
     /usr/local/lib
     /usr/lib64
     /usr/lib
    This usually results in installations to /usr/local/lib on Ubuntu and to
    /usr/local/lib64 on others.

--- sdk/userspace/install_fpga_mgmt_tools.sh
+++ sdk/userspace/install_fpga_mgmt_tools.sh
@@ -15,30 +15,79 @@
 # permissions and limitations under the License.
 #
 
-set -e
 
 if [ -z "$SDK_DIR" ]; then
-    echo "Error: SDK_DIR environment variable is not set.  Please 'source sdk_setup.sh' from the aws-fpga directory first."
-    exit 1
-fi  
+	echo "Error: SDK_DIR environment variable is not set.  Please 'source sdk_setup.sh' from the aws-fpga directory first."
+	exit 1
+fi
+
+if [ $EUID != 0 ]; then
+	echo ""
+	echo "Root privileges are required to install. You may be asked for your password..."
+	sudo -E "$0" "$@"
+	exit $?
+else
+	echo "Executing as root..."
+	echo ""
+fi
+
+BASE_PATH=/usr/local
+
+# check to see if the /usr/local/bin is on the sudo PATH (is secure_path enabled?)
+echo $PATH | grep "$BASE_PATH"
+ret=$?
+if [ $ret -ne "0" ]; then
+	BASE_PATH=/usr
+fi
 
 SDK_MGMT_DIR=$SDK_DIR/userspace
 AFI_MGMT_TOOLS_SRC_DIR=$SDK_MGMT_DIR/fpga_mgmt_tools/src
-AFI_MGMT_TOOLS_DST_DIR=/usr/bin
+AFI_MGMT_TOOLS_DST_DIR=$BASE_PATH/bin
 AFI_MGMT_TOOLS_LIB_DIR=$SDK_MGMT_DIR/lib/so
-AFI_MGMT_LIBS_DST_DIR=/usr/lib64
 
-# Build and install the Amazon FPGA Image (AFI) Management Tools
-cd $SDK_MGMT_DIR
-$SDK_MGMT_DIR/mkall_fpga_mgmt_tools.sh
+# in order to accommodate different distributions, check several options for the user-libraries directory
+if [ -d "/usr/local/lib64" ]; then
+	AFI_MGMT_LIBS_DST_DIR=/usr/local/lib64
+elif [ -d "/usr/local/lib" ]; then
+	AFI_MGMT_LIBS_DST_DIR=/usr/local/lib
+elif [ -d "/usr/lib64" ]; then
+	AFI_MGMT_LIBS_DST_DIR=/usr/lib64
+elif [ -d "/usr/lib" ]; then
+	AFI_MGMT_LIBS_DST_DIR=/usr/lib
+else
+	echo "Error: No directory for installing libraries."
+	exit 1
+fi
 
 if [ ! -d "$AFI_MGMT_TOOLS_DST_DIR" ]; then
-    mkdir -p $AFI_MGMT_TOOLS_DST_DIR
+	mkdir -p $AFI_MGMT_TOOLS_DST_DIR
 fi
 
 # /usr/bin requires sudo permissions 
 echo "AWS FPGA: Copying Amazon FPGA Image (AFI) Management Tools to $AFI_MGMT_TOOLS_DST_DIR"
-sudo cp -f $AFI_MGMT_TOOLS_SRC_DIR/fpga-* $AFI_MGMT_TOOLS_DST_DIR
-sudo cp -f $AFI_MGMT_TOOLS_LIB_DIR/libfpga_mgmt.so $AFI_MGMT_LIBS_DST_DIR
+cp -f $AFI_MGMT_TOOLS_SRC_DIR/fpga-* $AFI_MGMT_TOOLS_DST_DIR
+cp -f $AFI_MGMT_TOOLS_LIB_DIR/libfpga_mgmt.so.1.0.0 $AFI_MGMT_LIBS_DST_DIR
+ln -sf libfpga_mgmt.so.1 $AFI_MGMT_LIBS_DST_DIR/libfpga_mgmt.so
+
+echo "AWS FPGA: Installing shared library to $AFI_MGMT_LIBS_DST_DIR"
+ld_conf_change="0"
+while true; do
+	# update the dynamic linker cache
+	ldconfig
+
+	# confirm that the linker cache stored the library we want
+	ldconfig -p | grep "libfpga_mgmt\.so\.1"
+	ret=$?
+	if [ $ret -ne "0" ]; then
+		if [ "$ld_conf_change" -eq "1" ]; then
+			echo "Error: Unable to automatically install the fpga_mgmt library."
+			exit 1
+		fi
+		ld_conf_change="1"
+		echo "$AFI_MGMT_LIBS_DST_DIR" > /etc/ld.so.conf.d/fpga_mgmt-x86_64.conf
+	else
+		break
+	fi
+done
 
 echo "AWS FPGA: Done with Amazon FPGA Image (AFI) Management Tools install."
