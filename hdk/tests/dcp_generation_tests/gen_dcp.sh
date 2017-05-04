ea761d4cb1faddfbf1eac53093a15b39ce0f2352
Deep Patel <deeppat@amazon.com>
2017-05-04 16:46:50 -0500

    Adding size and tar file checks when creating dcps

--- hdk/tests/dcp_generation_tests/gen_dcp.sh
+++ hdk/tests/dcp_generation_tests/gen_dcp.sh
@@ -23,8 +23,11 @@ if [ "$test" = "" ]; then
     exit 1
 fi
 
+echo "INFO: Sourcing hdk_setup.sh"
 source $WORKSPACE/hdk_setup.sh;
 
+
+echo "INFO: Setting CL_DIR=$HDK_DIR/cl/examples/$test"
 export CL_DIR=$HDK_DIR/cl/examples/$test
 
 if [ ! -d $CL_DIR ]; then
@@ -32,5 +35,56 @@ if [ ! -d $CL_DIR ]; then
     exit 1
 fi
 
+echo "INFO: Running $HDK_DIR/cl/examples/$test/build/scripts/aws_build_dcp_from_cl.sh -foreground"
+
 cd $HDK_DIR/cl/examples/$test/build/scripts
 ./aws_build_dcp_from_cl.sh -foreground
+
+if [ $? -ne 0 ]; then
+        echo -e >&2 "ERROR: Non zero error code while generating DCP!";
+        exit 1
+fi
+
+echo "INFO: DCP Generation Finished"
+
+if [ ! -d $HDK_DIR/cl/examples/$test/build/checkpoints/to_aws ]; then
+    echo -e >&2 'ERROR: The checkpoints/to_aws directory does not exist! Maybe the checkpoint wasnt created?'
+    exit 1
+fi
+
+cd $HDK_DIR/cl/examples/$test/build/checkpoints/to_aws
+
+echo "INFO: Checking that a non zero size manifest file exists in the folder"
+
+non_zero_manifest=$(find . -name "*.manifest.txt" -type f ! -size 0)
+
+if [ "$non_zero_manifest" = "" ]; then
+    echo -e >&2 "ERROR: Manifest file not found or is of 0 byte size\n"
+    exit 1
+fi
+
+echo "INFO: Checking that a non zero size dcp file exists in the folder"
+
+non_zero_dcp=$(find . -name "*.dcp" -type f ! -size 0)
+
+if [ "$non_zero_dcp" = "" ]; then
+    echo -e >&2 "ERROR: DCP file not found or is of 0 byte size\n"
+    exit 1
+fi
+
+echo "INFO: Checking that a dcp exists in the tar file"
+/usr/bin/tar tvf *.Developer_CL.tar "*.dcp"
+
+if [ $? -ne 0 ]; then
+        echo -e >&2 "ERROR: Did not find dcp in the tar file!";
+        exit 1
+fi
+
+echo "INFO: Checking that a manifest exists in the tar file"
+/usr/bin/tar tvf *.Developer_CL.tar "*.manifest.txt"
+
+if [ $? -ne 0 ]; then
+        echo -e >&2 "ERROR: Did not find the manifest in the tar file!";
+        exit 1
+fi
+
