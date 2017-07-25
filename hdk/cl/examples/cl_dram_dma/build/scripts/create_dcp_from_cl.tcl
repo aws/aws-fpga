f1af00486a91b332319095531394d93b7faa2437
Carlos Cabral <cccabra@amazon.com>
2017-07-25 12:21:05 -0700

    Updating warning suppress directives.
    Change-Id: I6d03d591df011bdb28aad39dd0b048245ecefedd

--- hdk/cl/examples/cl_dram_dma/build/scripts/create_dcp_from_cl.tcl
+++ hdk/cl/examples/cl_dram_dma/build/scripts/create_dcp_from_cl.tcl
@@ -109,26 +109,61 @@ set_msg_config -string {AXI_QUAD_SPI} -suppress
 # These are to avoid warning messages that may not be real issues. A developer
 # may comment them out if they wish to see more information from warning
 # messages.
-set_msg_config -id {Constraints 18-550} -suppress
-set_msg_config -id {Constraints 18-619} -suppress
-set_msg_config -id {DRC 23-20}          -suppress
-set_msg_config -id {Physopt 32-742}     -suppress
-set_msg_config -id {Place 46-14}        -suppress
-set_msg_config -id {Synth 8-3295}       -suppress
-set_msg_config -id {Synth 8-3321}       -suppress
-set_msg_config -id {Synth 8-3331}       -suppress
-set_msg_config -id {Synth 8-3332}       -suppress
-set_msg_config -id {Synth 8-350}        -suppress
-set_msg_config -id {Synth 8-3848}       -suppress
-set_msg_config -id {Synth 8-3917}       -suppress
-set_msg_config -id {Timing 38-436}      -suppress
-set_msg_config -id {Synth 8-6014}       -suppress
-set_msg_config -id {Constraints 18-952} -suppress
-set_msg_config -id {DRC CKLD-2}         -suppress
-set_msg_config -id {DRC REQP-1853}      -suppress
-set_msg_config -id {Route 35-456}       -suppress
-set_msg_config -id {Route 35-455}       -suppress
-set_msg_config -id {Route 35-459}       -suppress
+
+set_msg_config -id {Common 17-55}        -suppress
+set_msg_config -id {Designutils 20-1567} -suppress
+set_msg_config -id {IP_Flow 19-2162}     -suppress
+set_msg_config -id {Project 1-498}       -suppress
+set_msg_config -id {Route 35-328}        -suppress
+set_msg_config -id {Vivado 12-508}       -suppress
+set_msg_config -id {Constraints 18-4866} -suppress
+set_msg_config -id {filemgmt 56-12}      -suppress
+set_msg_config -id {Constraints 18-4644} -suppress
+set_msg_config -id {Coretcl 2-64}        -suppress
+set_msg_config -id {Vivado 12-4739}      -suppress
+set_msg_config -id {Vivado 12-5201}      -suppress
+set_msg_config -id {DRC CKLD-1}          -suppress
+set_msg_config -id {IP_Flow 19-2248}     -suppress
+set_msg_config -id {Opt 31-155}          -suppress
+set_msg_config -id {Synth 8-115}         -suppress
+set_msg_config -id {Synth 8-3936}        -suppress
+set_msg_config -id {Vivado 12-1023}      -suppress
+set_msg_config -id {Constraints 18-550}  -suppress
+set_msg_config -id {Synth 8-3295}        -suppress
+set_msg_config -id {Synth 8-3321}        -suppress
+set_msg_config -id {Synth 8-3331}        -suppress
+set_msg_config -id {Synth 8-3332}        -suppress
+set_msg_config -id {Synth 8-350}         -suppress
+set_msg_config -id {Synth 8-3848}        -suppress
+set_msg_config -id {Synth 8-3917}        -suppress
+set_msg_config -id {Synth 8-6014}        -suppress
+set_msg_config -id {Vivado 12-1580}      -suppress
+set_msg_config -id {Constraints 18-619}  -suppress
+set_msg_config -id {DRC CKLD-2}          -suppress
+set_msg_config -id {DRC REQP-1853}       -suppress
+set_msg_config -id {Timing 38-436}       -suppress
+
+
+#set_msg_config -id {Constraints 18-550} -suppress
+#set_msg_config -id {Constraints 18-619} -suppress
+#set_msg_config -id {DRC 23-20}          -suppress
+#set_msg_config -id {Physopt 32-742}     -suppress
+#set_msg_config -id {Place 46-14}        -suppress
+#set_msg_config -id {Synth 8-3295}       -suppress
+#set_msg_config -id {Synth 8-3321}       -suppress
+#set_msg_config -id {Synth 8-3331}       -suppress
+#set_msg_config -id {Synth 8-3332}       -suppress
+#set_msg_config -id {Synth 8-350}        -suppress
+#set_msg_config -id {Synth 8-3848}       -suppress
+#set_msg_config -id {Synth 8-3917}       -suppress
+#set_msg_config -id {Timing 38-436}      -suppress
+#set_msg_config -id {Synth 8-6014}       -suppress
+#set_msg_config -id {Constraints 18-952} -suppress
+#set_msg_config -id {DRC CKLD-2}         -suppress
+#set_msg_config -id {DRC REQP-1853}      -suppress
+#set_msg_config -id {Route 35-456}       -suppress
+#set_msg_config -id {Route 35-455}       -suppress
+#set_msg_config -id {Route 35-459}       -suppress
 
 puts "AWS FPGA: ([clock format [clock seconds] -format %T]) Calling the encrypt.tcl.";
 
