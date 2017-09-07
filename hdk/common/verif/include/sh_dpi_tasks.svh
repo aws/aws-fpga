3cb691c5bb26f3636409e3f8e37432078a812634
AWShimasajj <himasajj@amazon.com>
2017-09-07 16:13:59 -0500

    Update sh_dpi_tasks.svh
    Fixed the typo

--- hdk/common/verif/include/sh_dpi_tasks.svh
+++ hdk/common/verif/include/sh_dpi_tasks.svh
@@ -393,7 +393,7 @@ end
              input logic [5:0] id = 6'h0); 
        logic [63:0] tmp;
        `SLOT_MACRO_TASK(peek(.addr(addr), .data(tmp), .id(id), .size(DataSize::UINT32), .intf(AxiPort::PORT_OCL)))
-       data = {32h'0, tmp[31:0]};
+      data = {32'h0, tmp[31:0]};
    endtask
 
    //=================================================
@@ -431,7 +431,7 @@ end
              input logic [5:0] id = 6'h0); 
        logic [63:0] tmp;
        `SLOT_MACRO_TASK(peek(.addr(addr), .data(data), .id(id), .size(DataSize::UINT32), .intf(AxiPort::PORT_SDA)))
-       data = {32h'0, tmp[31:0]};
+      data = {32'h0, tmp[31:0]};
    endtask
 
    //=================================================
@@ -469,7 +469,7 @@ end
              input logic [5:0] id = 6'h0); 
       logic [63:0] tmp;
       `SLOT_MACRO_TASK(peek(.addr(addr), .data(data), .id(id), .size(DataSize::UINT32), .intf(AxiPort::PORT_BAR1)))
-      data = {32h'0, tmp[31:0]};
+      data = {32'h0, tmp[31:0]};
    endtask
 
    function bit is_dma_to_cl_done(input int slot_id = 0, input int chan);
