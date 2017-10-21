46e0f110d35259ecc2dddd8b27731ae03251741c
Hima Sajja <himasajj@amazon.com>
2017-10-21 13:07:22 -0700

    Removed hello_world_api file
    Change-Id: I4a2478096ddb7f78e5df00603c5f4a9318593ef2

--- hdk/cl/examples/cl_hello_world/software/runtime/test_hello_world.c
+++ hdk/cl/examples/cl_hello_world/software/runtime/test_hello_world.c
@@ -27,11 +27,24 @@
 /* use the stdout logger for printing debug information  */
 #ifndef SV_TEST
 const struct logger *logger = &logger_stdout;
+/*
+ * pci_vendor_id and pci_device_id values below are Amazon's and avaliable to use for a given FPGA slot. 
+ * Users may replace these with their own if allocated to them by PCI SIG
+ */
+static uint16_t pci_vendor_id = 0x1D0F; /* Amazon PCI Vendor ID */
+static uint16_t pci_device_id = 0xF000; /* PCI Device ID preassigned by Amazon for F1 applications */
+
+/*
+ * check if the corresponding AFI for hello_world is loaded
+ */
 #endif
 
 //The MAIN will be different between Simulation and software, so a macro is defined. The macro definition can be found in the header file.
-MAIN {
-
+#ifdef SV_TEST
+void test_main(uint32_t *exit_code) {
+#else
+int main(int argc, char **argv) {  
+#endif
     //The statements within SCOPE ifdef below are needed for HW/SW co-simulation with VCS
     #ifdef SCOPE
       svScope scope;
@@ -50,7 +63,10 @@ MAIN {
 
     slot_id = 0;
 
+#ifndef SV_TEST 
     rc = check_afi_ready(slot_id);
+#endif
+
     fail_on(rc, out, "AFI not ready");
     
     /* Accessing the CL registers via AppPF BAR0, which maps to sh_cl_ocl_ AXI-Lite bus between AWS FPGA Shell and the CL*/
@@ -86,6 +102,60 @@ out:
    *exit_code = 0;
 #endif
 }
+
+/* As HW simulation test is not run on a AFI, the below function is not valid */
+#ifndef SV_TEST
+
+int check_afi_ready(int slot_id) {
+    struct fpga_mgmt_image_info info = {0}; 
+    int rc;
+
+    /* get local image description, contains status, vendor id, and device id. */
+    rc = fpga_mgmt_describe_local_image(slot_id, &info,0);
+    fail_on(rc, out, "Unable to get AFI information from slot %d. Are you running as root?",slot_id);
+
+    /* check to see if the slot is ready */
+    if (info.status != FPGA_STATUS_LOADED) {
+        rc = 1;
+        fail_on(rc, out, "AFI in Slot %d is not in READY state !", slot_id);
+    }
+
+    printf("AFI PCI  Vendor ID: 0x%x, Device ID 0x%x\n",
+        info.spec.map[FPGA_APP_PF].vendor_id,
+        info.spec.map[FPGA_APP_PF].device_id);
+
+    /* confirm that the AFI that we expect is in fact loaded */
+    if (info.spec.map[FPGA_APP_PF].vendor_id != pci_vendor_id ||
+        info.spec.map[FPGA_APP_PF].device_id != pci_device_id) {
+        printf("AFI does not show expected PCI vendor id and device ID. If the AFI "
+               "was just loaded, it might need a rescan. Rescanning now.\n");
+
+        rc = fpga_pci_rescan_slot_app_pfs(slot_id);
+        fail_on(rc, out, "Unable to update PF for slot %d",slot_id);
+        /* get local image description, contains status, vendor id, and device id. */
+        rc = fpga_mgmt_describe_local_image(slot_id, &info,0);
+        fail_on(rc, out, "Unable to get AFI information from slot %d",slot_id);
+
+        printf("AFI PCI  Vendor ID: 0x%x, Device ID 0x%x\n",
+            info.spec.map[FPGA_APP_PF].vendor_id,
+            info.spec.map[FPGA_APP_PF].device_id);
+
+        /* confirm that the AFI that we expect is in fact loaded after rescan */
+        if (info.spec.map[FPGA_APP_PF].vendor_id != pci_vendor_id ||
+             info.spec.map[FPGA_APP_PF].device_id != pci_device_id) {
+            rc = 1;
+            fail_on(rc, out, "The PCI vendor id and device of the loaded AFI are not "
+                             "the expected values.");
+        }
+    }
+    
+    return rc;
+
+out:
+    return 1;
+}
+
+#endif
 
 /*
  * An example to attach to an arbitrary slot, pf, and bar with register access.
