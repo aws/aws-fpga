3624bd0195b6c4141aca183c5714c67d97662f22
Deep Patel <deeppat@amazon.com>
2019-08-19 14:28:08 -0500

    Return instead of exit if we don't find the right version (#567)

--- sdaccel_setup.sh
+++ sdaccel_setup.sh
@@ -184,7 +184,7 @@ if [[ $RELEASE_VER =~ .*2017\.4.* || $RELEASE_VER =~ .*2018\.2.* || $RELEASE_VER
     ln -sf $SDACCEL_DIR/examples/xilinx_$RELEASE_VER $SDACCEL_DIR/examples/xilinx
 else
    echo " $RELEASE_VER is not supported (2017.4, 2018.2 & 2018.3 are supported).\n"
-   exit 2
+   return 2
 fi
 
 # settings64 removal - once we put this in the AMI, we will add a check
