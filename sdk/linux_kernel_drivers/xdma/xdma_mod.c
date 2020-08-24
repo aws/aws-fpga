3aaec77a8f57436c7eeef5f1a5cf18834fff12a8
Karen Xie <karen.xie@xilinx.com>
2020-08-24 15:42:43 -0700

    xdma: add back F1 specific device ids

--- sdk/linux_kernel_drivers/xdma/xdma_mod.c
+++ sdk/linux_kernel_drivers/xdma/xdma_mod.c
@@ -105,8 +105,8 @@ static const struct pci_device_id pci_ids[] = {
 	{ PCI_DEVICE(0x10ee, 0x4B28), },
 
 	{ PCI_DEVICE(0x10ee, 0x2808), },
-	{ PCI_DEVICE(0x10ee, 0xf000), },
-	{ PCI_DEVICE(0x10ee, 0xf001), },
+	{ PCI_DEVICE(0x1d0f, 0xf000), },
+	{ PCI_DEVICE(0x1d0f, 0xf001), },
 
 #ifdef INTERNAL_TESTING
 	{ PCI_DEVICE(0x1d0f, 0x1042), 0},
