882d7561e24c7ac5c565acb70e54044102aeef35
Deep Patel <deeppat@amazon.com>
2021-01-20 09:14:57 -0600

    Bringing in external commits from aws-fpga (#632)
    * Fixed failing cl_hello_world_vhdl example (#510)
    
    Co-authored-by: Joost Hoozemans <j.j.hoozemans@tudelft.nl>
    
    * Add peek and poke for the 3 other AXI-Lite interfaces (#511)
    
    * Added peek and poke for the BAR1 interface
    
    * Added peek and poke for the SDA and PCIS AXI slave interfaces.
    
    Now all the 4 slave interface have a specific set of peek/poke functions callable from C.
    The new cl_peek_ocl and cl_poke_ocl are actually the same as the original cl_peek/cl_poke,
    which are still needed because they are used internally to configure the DDR controllers and test circuits.
    
    Co-authored-by: Joost Hoozemans <j.j.hoozemans@tudelft.nl>
    
    Co-authored-by: Joost Hoozemans <joosthooz@msn.com>
    Co-authored-by: Joost Hoozemans <j.j.hoozemans@tudelft.nl>

--- sdk/userspace/include/utils/sh_dpi_tasks.h
+++ sdk/userspace/include/utils/sh_dpi_tasks.h
@@ -33,6 +33,14 @@ extern void sv_map_host_memory(uint8_t *memory);
 
 extern void cl_peek(uint64_t addr, uint32_t *data);
 extern void cl_poke(uint64_t addr, uint32_t  data);
+extern void cl_peek_pcis(uint64_t addr, uint32_t *data);
+extern void cl_poke_pcis(uint64_t addr, uint32_t  data);
+extern void cl_peek_sda(uint64_t addr, uint32_t *data);
+extern void cl_poke_sda(uint64_t addr, uint32_t  data);
+extern void cl_peek_ocl(uint64_t addr, uint32_t *data);
+extern void cl_poke_ocl(uint64_t addr, uint32_t  data);
+extern void cl_peek_bar1(uint64_t addr, uint32_t *data);
+extern void cl_poke_bar1(uint64_t addr, uint32_t  data);
 extern void sv_int_ack(uint32_t int_num);
 extern void sv_pause(uint32_t x);
 extern void sv_fpga_start_buffer_to_cl(uint32_t slot_id, uint32_t chan, uint32_t buf_size, uint64_t wr_buffer_addr, uint64_t cl_addr);
