7a0d9fc335bac24b6e4cec928987038e3c73cbad
Jiaxun Yang <jiaxun.yang@flygoat.com>
2026-04-11 10:56:43 +0100

    CL_TEMPLATE/scripts: Source common scripts
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- hdk/cl/examples/CL_TEMPLATE/build/scripts/aws_build_dcp_from_cl.py
+++ hdk/cl/examples/CL_TEMPLATE/build/scripts/aws_build_dcp_from_cl.py
@@ -1,1 +1,23 @@
-./../../../../../common/shell_stable/build/scripts/aws_build_dcp_from_cl.py
+#!/usr/bin/env python3
+
+import os
+import sys
+
+
+def main() -> int:
+    hdk_shell_dir = os.environ.get("HDK_SHELL_DIR")
+    if not hdk_shell_dir:
+        sys.stderr.write("HDK_SHELL_DIR is not set. Please source hdk_setup.sh.\n")
+        return 127
+
+    candidate = os.path.join(hdk_shell_dir, "build", "scripts", "aws_build_dcp_from_cl.py")
+    if os.path.isfile(candidate) and os.access(candidate, os.X_OK):
+        os.execv(candidate, [candidate, *sys.argv[1:]])
+
+    sys.stderr.write("Unable to locate aws_build_dcp_from_cl.py from the AWS HDK environment.\n")
+    sys.stderr.write("Please source hdk_setup.sh so HDK_SHELL_DIR points to the active shell.\n")
+    return 127
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
--- hdk/cl/examples/CL_TEMPLATE/build/scripts/build_all.tcl
+++ hdk/cl/examples/CL_TEMPLATE/build/scripts/build_all.tcl
@@ -1,1 +1,7 @@
-../../../../../common/shell_stable/build/scripts/build_all.tcl
+if { [info exists ::env(HDK_SHELL_DIR)] } {
+  set common_build_all [file join $::env(HDK_SHELL_DIR) build scripts build_all.tcl]
+} else {
+  error "HDK_SHELL_DIR not set. Please source hdk_setup.sh before sourcing build_all.tcl."
+}
+
+source $common_build_all
